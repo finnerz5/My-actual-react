55cb4f5f630ef3eab85d2fa25cf1dcfa7052d62a
finley <144432889+finnerz5@users.noreply.github.com>
2023-11-23 14:44:08 +0000

    commi

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Feedbackpage from "./Pages/FeedbackPage";
 import Aboutpage from "./Pages/AboutPage";
 import Helppage from "./Pages/HelpPage";
 import Navbar from "./Pages/NavBar";
+import DataForm from "./Pages/UserDataForm";
 function App() {
   return (
     <>
@@ -28,6 +29,8 @@ function App() {
           <Route path={NOT_FOUND_PATH} element={<Notfound />} />
         </Route>
       </Routes>
+
+      <DataForm/>
      
     </>
   );
--- src/Pages/UserDataForm.tsx
+++ src/Pages/UserDataForm.tsx
@@ -1,15 +1,42 @@
-import { useContext, useState } from "react";
+import { FormEvent, useContext, useState } from "react";
 import { UserDataContext } from "./UserDataProvider";
 
-function dataForm(){
-    const [ localData, setLocalData ] = useState("");
+function DataForm(){
+    const [ localName, setLocalname ] = useState("");
+    const [ localAge, setLocalAge] = useState(0);
+    const [ localAddress, setlocalAddress ] = useState("");
     const dataContext= useContext(UserDataContext);
 
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
-        dataContext?.setUserData(localData);
-        setLocalData("");
+        const userData = {
+            name: localName,
+            age: localAge,
+            address: localAddress
+        }
+        dataContext?.setUserData(userData);
     }
 
+    return(
+        <>
+
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="nameInput">Name:</label>
+                <label htmlFor="ageInput">Age:</label>
+                <label htmlFor="addressInput">Address:</label>
+                <input type="text" id="nameInput" autoComplete="off" onChange={(e) => setLocalname(e.target.value)} value={localName} required/>
+                <input type="text" id="ageInput" autoComplete="off" onChange={(e) => setLocalAge(parseInt(e.target.value))} value={localAge} required/>
+                <input type="text" id="addressInput" autoComplete="off" onChange={(e) => setlocalAddress(e.target.value)} value={localAddress} required/>
+                <button type="submit">Submit</button>
+                
+            </form>
+            <p>Local name is {localName}</p>
+            <p>Local age is {localAge}</p>
+            <p>Local address is {localAddress}</p>
+
+        </>
+    )
 
 }
+
+export default DataForm;
